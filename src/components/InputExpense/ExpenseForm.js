633344ab8172a6b7942f3aa5d861566cf5564972
CaseyRay08 <edenscasey89@gmail.com>
2022-11-11 16:00:37 -0500

    added form functionality

--- src/components/InputExpense/ExpenseForm.js
+++ src/components/InputExpense/ExpenseForm.js
@@ -19,6 +19,13 @@ export const ExpenseForm = (props) => {
         setCost(e.target.value)
     }
 
+    const hideForm = () => {
+        setShowForm(false);
+        setDesc('');
+        setDate('');
+        setCost('');
+    }
+
 
     const handleForm = e => {
         e.preventDefault()
@@ -29,45 +36,43 @@ export const ExpenseForm = (props) => {
         }
 
         props.onExpenseData(expenseData)
-        setDesc('');
-        setDate('');
-        setCost('');
+
+        hideForm();
+
+
+
     };
-    if (showForm === false) {
-        return <button onClick={() => setShowForm(true)}>Add Expense</button>
-    }
     return (
-        // {
-        //     show
-        // }
+        <div>
+            {
+                showForm ? (
+                    <form onSubmit={handleForm} >
+                        <div className='input-expense__controls'>
 
-        < form onSubmit={handleForm} >
+                            <div className='input-expense__controls'>
+                                <label>Description</label>
+                                <input type='text' onChange={handleDesc} value={desc} />
+                            </div>
 
-            <div className='input-expense__controls'>
+                            <div className='input-expense__controls'>
+                                <label>Date</label>
+                                <input type='date' min='2015-01-01' max='2025-12-31' onChange={handleDate} value={date} />
+                            </div>
 
-                <div className='input-expense__controls'>
-                    <label>Description</label>
-                    <input type='text' onChange={handleDesc} value={desc} />
-                </div>
+                            <div className='input-expense__controls'>
+                                <label>Cost</label>
+                                <input type='number' min='0.01' step='0.01' onChange={handleCost} value={cost} />
+                            </div>
 
-                <div className='input-expense__controls'>
-                    <label>Date</label>
-                    <input type='date' min='2015-01-01' max='2025-12-31' onChange={handleDate} value={date} />
-                </div>
+                        </div>
 
-                <div className='input-expense__controls'>
-                    <label>Cost</label>
-                    <input type='number' min='0.01' step='0.01' onChange={handleCost} value={cost} />
-                </div>
-
-            </div>
-
-            <div className='new-expense__actions'>
-                <button onClick={() => setShowForm(false)}>Cancel</button>
-                <button type='submit'>Add Expense</button>
-            </div>
-        </form >
-
+                        <div className='new-expense__actions'>
+                            <button onClick={() => hideForm()}>Cancel</button>
+                            <button type='submit'>Add Expense</button>
+                        </div>
+                    </form >
+                ) : <button onClick={() => setShowForm(true)}>Add Expense</button>
+            }
+        </div>
     )
-
 }
